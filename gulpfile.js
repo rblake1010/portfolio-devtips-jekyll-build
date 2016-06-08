76717145d8ed9cf22283cce2ea5d7ffda67873bf
Richard <rblake@metrostarstystems.com>
2016-06-08 11:24:05 -0400

    created nav mixin and removed from yml

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ gulp.task('sass', function () {
             includePaths: ['css'],
             onError: browserSync.notify
         }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        //.pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('_site/assets/css'))
         .pipe(browserSync.reload({stream:true}))
         .pipe(gulp.dest('assets/css'));
